1dd5a17ff6623dd07e0d6f5d642fa4b006854a4d
hunaiqiang <1451939833@qq.com>
2020-07-30 01:05:33 +0800

    把本地工程添加到git

---
+++ git-first-project/src/com/offen/FirstClass.java
@@ -0,0 +1,10 @@
+package com.offen;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+        System.out.println("nihao");
+        System.out.println("nihao");
+        System.out.println("nihao");
+    }
+}
